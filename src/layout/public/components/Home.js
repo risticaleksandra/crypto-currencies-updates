6967e85b6ee47ad627825c14c8c36594d04b57a6
Aleksandra Ristic <ristic.aleksandra1990@gmail.com>
2020-07-05 19:19:32 +0200

    handle navigation link selected - bold selected link

--- src/layout/public/components/Home.js
+++ src/layout/public/components/Home.js
@@ -12,24 +12,25 @@ const tabelHeaders = [
 const Home = ({ tickers }) => {
   return useMemo(() => {
     return (
-        <BootstrapTable
-          keyField="index"
-          data={Object.values(tickers).map((row, index) => ({
-            ...row,
-            index: index + 1,
-          }))}
-          hover={true}
-        >
-          {tabelHeaders.map((column, index) => (
-            <TableHeaderColumn
-              dataField={column.value}
-              dataSort={true}
-              width="20%"
-            >
-              {column.name}
-            </TableHeaderColumn>
-          ))}
-        </BootstrapTable>
+      <BootstrapTable
+        keyField="index"
+        data={Object.values(tickers).map((row, index) => ({
+          ...row,
+          index: index + 1,
+        }))}
+        hover={true}
+      >
+        {tabelHeaders.map((column, index) => (
+          <TableHeaderColumn
+            key={index}
+            dataField={column.value}
+            dataSort={true}
+            width="20%"
+          >
+            {column.name}
+          </TableHeaderColumn>
+        ))}
+      </BootstrapTable>
     );
   }, [tickers]);
 };
--- src/layout/public/components/Navigation.js
+++ src/layout/public/components/Navigation.js
@@ -1,20 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Navbar, Nav, Button } from "react-bootstrap";
 import { setLoginFlag, isLoggedIn } from "../../../web_services/login_services";
+import { useHistory } from "react-router";
 
 const Navigation = () => {
+  const history = useHistory();
   const [loggedIn, setLoggedIn] = useState(isLoggedIn());
+  const [homeLinkActive, setHomeLinkActive] = useState(true);
+  const [profileLinkActive, setProfileLinkActive] = useState(false);
+
+  useEffect(() => {
+    history.location.pathname === "/home"
+      ? setActiveLink(true, false)
+      : setActiveLink(false, true);
+  }, [history]);
+
   const hanldeUserLogIn = () => {
     setLoginFlag(true);
     setLoggedIn(true);
   };
+
+  const setActiveLink = (homeLinkActive, profileLinkActive) => {
+    console.log(homeLinkActive, profileLinkActive);
+    setHomeLinkActive(homeLinkActive);
+    setProfileLinkActive(profileLinkActive);
+  };
+
   return (
     <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav.Link href="/home">Home</Nav.Link>
-          {loggedIn ? <Nav.Link href="/profile">Profile</Nav.Link> : null}
+          <Nav.Link active={homeLinkActive} href="/home">
+            Home
+          </Nav.Link>
+          {loggedIn ? (
+            <Nav.Link active={profileLinkActive} href="/profile">
+              Profile
+            </Nav.Link>
+          ) : null}
         </Nav>
         <Button variant="primary" onClick={() => hanldeUserLogIn()}>
           Login
